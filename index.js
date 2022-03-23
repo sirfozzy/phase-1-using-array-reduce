941196553f6823b020524cce1e99ca7903ac64fa
Jason Antman <antmanjt@gmail.com>
2022-03-22 23:33:23 -0400

    commit tree 54c6135f08eedb94e3b9e53c7a1360f85645989b

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+function reduce () {
+}
+
+function batteries () {
+}
+
+function totalBatteries(batteries) {
+    let totalBatteries= 31;
+    for (const batteryBatches of batteries) {
+    totalBatteries += batteryBatches;
+    };
+    return totalBatteries;
+}
